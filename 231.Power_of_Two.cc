7b15736e5dd6eb2e0b8ae7f8b820ac939b4df78c
EvanPai <evan105pai@gmail.com>
2024-03-20 13:30:05 +0800

    231.Power_of_Two.cc

---
+++ 231.Power_of_Two.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+
+// key : naive,bit manipulation
+
+/*  explain:
+    you can just use the naive solution(Solution1), if the test require other method,
+    try use the AND operator method(Solution2).
+*/
+
+class Solution1 {
+public:
+    bool isPowerOfTwo(int n) {
+        // naive
+        if(n<=0) return false;
+        if(n==1) return true;
+
+        long long num = 1;
+        bool ans = false;
+        while(num < n){
+            num *= 2;
+            
+            if(num == n) ans = true;
+        }
+
+        return ans;
+
+        // AND operator
+        return (n > 0 && (n & (n-1) == 0)) ? true : false;
+    }
+};
+
+class Solution2 {
+public:
+    bool isPowerOfTwo(int n) {
+        // AND operator
+        return (n > 0 && (n & (n-1)) == 0) ? true : false;
+    }
+};
