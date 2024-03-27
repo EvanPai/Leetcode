457f9fa353996e78faf01700d516b3cffd484105
EvanPai <evan105pai@gmail.com>
2024-03-27 23:22:08 +0800

    27.Remove_Element.cc

--- 64.Minimum_Path_Sum.cc
+++ 64.Minimum_Path_Sum.cc
@@ -9,27 +9,26 @@ using namespace std;
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
         dp[0][0] = grid[0][0];
 
-        for(int i=1; i<m; i++){
-            dp[i][0] = grid[i][0] + dp[i-1][0];
+        for(int i=1; i<rows; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
         }
 
-        for(int i=1; i<n; i++){
-            dp[0][i] = grid[0][i] + dp[0][i-1];
+        for(int j=1; j<cols; j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
         }
 
-        for(int i = 1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j]; 
+        for(int i=1; i<rows; i++){
+            for(int j=1; j<cols; j++){
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
             }
         }
 
-        return dp[m-1][n-1];
+        return dp[rows-1][cols-1];
     }
 };
